85e3bb8911eaf3284e9b2f783a7632071c175e45
OM Miles <miles@ommiles.com>
2021-07-20 13:28:44 -0700

    complete

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
 
 function largestSubarraySum(array){
-    // code to write here
+    let maxSum = 0
+    let currentSum = 0
+
+    for(let i = 0; i < array.length; i++){ 
+        currentSum = Math.max(array[i], currentSum + array[i])
+        maxSum = Math.max(currentSum, maxSum)
+    }
+    return maxSum
 }
 
 largestSubarraySum(array)
-// 16
